604d9f1bf748ac177ae8e942c3d2572ff6834de7
tzu <floya9733@gmail.com>
2025-11-27 23:01:57 +0800

    feat: 新增動態城市天氣查詢功能，更新路由與相關註解

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const axios = require("axios");
 
 // CWA API 設定
@@ -115,6 +116,152 @@ const getKaohsiungWeather = async (req, res) => {
   }
 };
 
+/**
+ * 動態取得城市天氣預報
+ * 支援多城市查詢
+ */
+// 前端協調好的縣市英文名稱對照
+const cityMap = {
+  taipei: "臺北市",
+  Taipei: "臺北市",
+  taoyuan: "桃園市",
+  Taoyuan: "桃園市",
+  taichung: "臺中市",
+  Taichung: "臺中市",
+  tainan: "臺南市",
+  Tainan: "臺南市",
+  kaohsiung: "高雄市",
+  Kaohsiung: "高雄市",
+  keelung: "基隆市",
+  Keelung: "基隆市",
+  hsinchu: "新竹市",
+  Hsinchu: "新竹市",
+  newtaipei: "新北市",
+  NewTaipei: "新北市",
+  hsinchucounty: "新竹縣",
+  HsinchuCounty: "新竹縣",
+  miaoli: "苗栗縣",
+  Miaoli: "苗栗縣",
+  changhua: "彰化縣",
+  Changhua: "彰化縣",
+  nantou: "南投縣",
+  Nantou: "南投縣",
+  yunlin: "雲林縣",
+  Yunlin: "雲林縣",
+  chiayi: "嘉義市",
+  Chiayi: "嘉義市",
+  chiayicounty: "嘉義縣",
+  ChiayiCounty: "嘉義縣",
+  pingtung: "屏東縣",
+  Pingtung: "屏東縣",
+  yilan: "宜蘭縣",
+  Yilan: "宜蘭縣",
+  hualien: "花蓮縣",
+  Hualien: "花蓮縣",
+  taitung: "臺東縣",
+  Taitung: "臺東縣",
+  penghu: "澎湖縣",
+  Penghu: "澎湖縣",
+  kinmen: "金門縣",
+  Kinmen: "金門縣",
+  lienchiang: "連江縣",
+  Lienchiang: "連江縣",
+};
+
+const getWeatherByCity = async (req, res) => {
+  try {
+    if (!CWA_API_KEY) {
+      return res.status(500).json({
+        error: "伺服器設定錯誤",
+        message: "請在 .env 檔案中設定 CWA_API_KEY",
+      });
+    }
+
+    const cityParam = req.params.city.toLowerCase();
+    const cwaCity = cityMap[cityParam];
+    if (!cwaCity) {
+      return res.status(400).json({ error: "不支援的城市" });
+    }
+
+    const response = await axios.get(
+      `${CWA_API_BASE_URL}/v1/rest/datastore/F-C0032-001`,
+      {
+        params: {
+          Authorization: CWA_API_KEY,
+          locationName: cwaCity,
+        },
+      }
+    );
+
+    const locationData = response.data.records.location[0];
+    if (!locationData) {
+      return res.status(404).json({
+        error: "查無資料",
+        message: `無法取得${cwaCity}天氣資料`,
+      });
+    }
+
+    const weatherData = {
+      city: locationData.locationName,
+      updateTime: response.data.records.datasetDescription,
+      forecasts: [],
+    };
+    const weatherElements = locationData.weatherElement;
+    const timeCount = weatherElements[0].time.length;
+    for (let i = 0; i < timeCount; i++) {
+      const forecast = {
+        startTime: weatherElements[0].time[i].startTime,
+        endTime: weatherElements[0].time[i].endTime,
+        weather: "",
+        rain: "",
+        minTemp: "",
+        maxTemp: "",
+        comfort: "",
+        windSpeed: "",
+      };
+      weatherElements.forEach((element) => {
+        const value = element.time[i].parameter;
+        switch (element.elementName) {
+          case "Wx":
+            forecast.weather = value.parameterName;
+            break;
+          case "PoP":
+            forecast.rain = value.parameterName + "%";
+            break;
+          case "MinT":
+            forecast.minTemp = value.parameterName + "°C";
+            break;
+          case "MaxT":
+            forecast.maxTemp = value.parameterName + "°C";
+            break;
+          case "CI":
+            forecast.comfort = value.parameterName;
+            break;
+          case "WS":
+            forecast.windSpeed = value.parameterName;
+            break;
+        }
+      });
+      weatherData.forecasts.push(forecast);
+    }
+    res.json({ success: true, data: weatherData });
+  } catch (error) {
+    console.error("取得天氣資料失敗:", error.message);
+    if (error.response) {
+      return res.status(error.response.status).json({
+        error: "CWA API 錯誤",
+        message: error.response.data.message || "無法取得天氣資料",
+        details: error.response.data,
+      });
+    }
+    res.status(500).json({
+      error: "伺服器錯誤",
+      message: "無法取得天氣資料，請稍後再試",
+    });
+  }
+};
+
 module.exports = {
   getKaohsiungWeather,
+  getWeatherByCity,
 };
--- routes/weather.js
+++ routes/weather.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const weatherController = require("../controllers/weatherController");
 
-// 取得高雄天氣預報
-router.get("/kaohsiung", weatherController.getKaohsiungWeather);
+// 動態取得城市天氣預報
+router.get("/:city", weatherController.getWeatherByCity);
 
 module.exports = router;
